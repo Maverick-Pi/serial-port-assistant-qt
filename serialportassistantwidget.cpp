bf2675a4fec7067f5361a259b0279f195edc6c85
maverick_pi <pimingjie@gmail.com>
2025-10-24 13:11:51 +0800

    feat: 🎸 Automatic detection of serial ports

--- serialportassistantwidget.cpp
+++ serialportassistantwidget.cpp
@@ -2,6 +2,7 @@
 #include "./ui_serialportassistantwidget.h"
 
 #include <QSerialPortInfo>
+#include <QTimer>
 
 SerialPortAssistantWidget::SerialPortAssistantWidget(QWidget *parent)
     : QWidget(parent)
@@ -11,21 +12,35 @@ SerialPortAssistantWidget::SerialPortAssistantWidget(QWidget *parent)
 
     serialPort = new QSerialPort(this);
 
-    QList<QSerialPortInfo> serialPortInfoList = QSerialPortInfo::availablePorts();
-    for (const QSerialPortInfo &i : std::as_const(serialPortInfoList)) {
-        ui->comboBox_serialPort->addItem(i.portName());
-    }
+    // 初始化映射表
+    configMap();
+
+    // 初始化串口列表
+    updateSerialPortList();
+
+    // 创建并启动定时器，每秒检测一次串口变化
+    serialPortCheckTimer = new QTimer(this);
+    connect(serialPortCheckTimer, &QTimer::timeout, this, &SerialPortAssistantWidget::checkSerialPorts);
+    serialPortCheckTimer->start(1000);
 
     connect(ui->btn_switch, &QPushButton::clicked, this, &SerialPortAssistantWidget::switchSerialPort);
 }
 
 SerialPortAssistantWidget::~SerialPortAssistantWidget()
 {
+    if (serialPortCheckTimer) {
+        serialPortCheckTimer->stop();
+        delete serialPortCheckTimer;
+    }
     delete ui;
     delete serialPort;
 }
 
-void SerialPortAssistantWidget::ConfigMap()
+/**
+ * @brief SerialPortAssistantWidget::configMap
+ * 串口配置映射
+ */
+void SerialPortAssistantWidget::configMap()
 {
     // 校验位枚举映射
     parityMap["None"] = QSerialPort::NoParity;
@@ -40,6 +55,30 @@ void SerialPortAssistantWidget::ConfigMap()
 }
 
 /**
+ * @brief SerialPortAssistantWidget::updateSerialPortList
+ * 更新串口列表
+ */
+void SerialPortAssistantWidget::updateSerialPortList()
+{
+    ui->comboBox_serialPort->clear();
+    previousPortList.clear();
+
+    QList<QSerialPortInfo> serialPortInfoList = QSerialPortInfo::availablePorts();
+    for (const QSerialPortInfo &i : std::as_const(serialPortInfoList)) {
+        ui->comboBox_serialPort->addItem(i.portName());
+        previousPortList.append(i.portName());
+    }
+
+    // 如果没有可用串口，可以添加一个提示项
+    if (previousPortList.isEmpty()) {
+        ui->btn_switch->setDisabled(true);
+        ui->comboBox_serialPort->addItem("无可用串口");
+    } else {
+        ui->btn_switch->setDisabled(false);
+    }
+}
+
+/**
  * 槽函数的定义
  */
 
@@ -49,6 +88,10 @@ void SerialPortAssistantWidget::ConfigMap()
  */
 void SerialPortAssistantWidget::switchSerialPort()
 {
+    // 如果当前没有可用串口，将按钮 disable，同时直接返回
+    if (previousPortList.isEmpty()) {
+        return;
+    }
     // 选择端口号
     serialPort->setPortName(ui->comboBox_serialPort->currentText());
     // 配置波特率
@@ -67,5 +110,52 @@ void SerialPortAssistantWidget::switchSerialPort()
         ui->btn_switch->setText(tr("关闭串口"));
     } else {
         qFatal() << "串口打开失败！";
+    }
+}
+
+/**
+ * @brief SerialPortAssistantWidget::checkSerialPorts
+ * 检测串口
+ */
+void SerialPortAssistantWidget::checkSerialPorts()
+{
+    QStringList currentPortList;
+
+    // 获取当前的可用串口列表
+    QList<QSerialPortInfo> serialPortInfoList = QSerialPortInfo::availablePorts();
+    for (const QSerialPortInfo &i : std::as_const(serialPortInfoList)) {
+        currentPortList.append(i.portName());
+    }
+
+    // 如果串口列表发生变化
+    if (currentPortList != previousPortList) {
+        qDebug() << "串口列表发生变化，更新列表...";
+
+        // 保存当前选中的串口
+        QString currentSelectedPort = ui->comboBox_serialPort->currentText();
+
+        // 更新串口列表
+        ui->comboBox_serialPort->clear();
+
+        for (const QString &portName : std::as_const(currentPortList)) {
+            ui->comboBox_serialPort->addItem(portName);
+        }
+
+        // 如果没有可用接口，添加提示
+        if (currentPortList.isEmpty()) {
+            ui->btn_switch->setDisabled(true);
+            ui->comboBox_serialPort->addItem(tr("无可用串口"));
+        }
+        // 如果之前选中的串口仍然存在，则重新选中它
+        else if (currentPortList.contains(currentSelectedPort)) {
+            ui->comboBox_serialPort->setCurrentText(currentSelectedPort);
+            ui->btn_switch->setDisabled(false);
+        } else {
+            ui->btn_switch->setDisabled(false);
+        }
+
+        // 更新记录的上次串口列表
+        previousPortList = currentPortList;
+        qDebug() << "当前可用串口：" << currentPortList;
     }
 }
--- serialportassistantwidget.h
+++ serialportassistantwidget.h
@@ -20,12 +20,17 @@ public:
 
 private:
     Ui::SerialPortAssistantWidget *ui;
-    QSerialPort* serialPort;
-    QHash<QString, QSerialPort::Parity> parityMap;
-    QHash<QString, QSerialPort::StopBits> stopBitsMap;
-    void ConfigMap();
+    QSerialPort* serialPort; 	// 串口实例
+    QHash<QString, QSerialPort::Parity> parityMap; 	// 串口校验位选项映射
+    QHash<QString, QSerialPort::StopBits> stopBitsMap; 	// 串口停止位选项映射
+    QTimer* serialPortCheckTimer; 	// 串口检测定时器
+    QStringList previousPortList; 	// 上一次检测到的串口列表
+
+    void configMap(); 	// 串口配置映射
+    void updateSerialPortList(); 	// 更新串口列表
 
 private slots:
     void switchSerialPort();
+    void checkSerialPorts(); 	// 定时检测串口
 };
 #endif // SERIALPORTASSISTANTWIDGET_H
