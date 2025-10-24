c9fca417f96891cbe0f64c7e80775a59bee463cb
maverick_pi <pimingjie@gmail.com>
2025-10-25 00:05:11 +0800

    feat: 🎸 Transmit and receive

--- serialportassistantwidget.cpp
+++ serialportassistantwidget.cpp
@@ -1,6 +1,8 @@
 #include "serialportassistantwidget.h"
 #include "./ui_serialportassistantwidget.h"
 
+#include <QDateTime>
+#include <QMessageBox>
 #include <QSerialPortInfo>
 #include <QTimer>
 
@@ -11,6 +13,10 @@ SerialPortAssistantWidget::SerialPortAssistantWidget(QWidget *parent)
     ui->setupUi(this);
 
     serialPort = new QSerialPort(this);
+    // 初始化发送数据字节总数
+    transmittedBytesTotal = 0;
+    // 初始化接收数据字节总数
+    receivedBytesTotal = 0;
 
     // 初始化映射表
     configMap();
@@ -24,6 +30,8 @@ SerialPortAssistantWidget::SerialPortAssistantWidget(QWidget *parent)
     serialPortCheckTimer->start(1000);
 
     connect(ui->btn_switch, &QPushButton::clicked, this, &SerialPortAssistantWidget::switchSerialPort);
+    connect(ui->btn_transmit, &QPushButton::clicked, this, &SerialPortAssistantWidget::transmitData);
+    connect(serialPort, &QSerialPort::readyRead, this, &SerialPortAssistantWidget::readyReadSerialPort);
 }
 
 SerialPortAssistantWidget::~SerialPortAssistantWidget()
@@ -79,6 +87,21 @@ void SerialPortAssistantWidget::updateSerialPortList()
 }
 
 /**
+ * @brief SerialPortAssistantWidget::enableSerialPortConfig
+ * 串口配置选项使能
+ * @param en 使能控制
+ */
+void SerialPortAssistantWidget::enabledSerialPortConfig(bool en)
+{
+    ui->comboBox_serialPort->setEnabled(en);
+    ui->comboBox_baud->setEnabled(en);
+    ui->comboBox_dataBit->setEnabled(en);
+    ui->comboBox_check->setEnabled(en);
+    ui->comboBox_stop->setEnabled(en);
+    ui->comboBox_flowCtrl->setEnabled(en);
+}
+
+/**
  * 槽函数的定义
  */
 
@@ -88,10 +111,17 @@ void SerialPortAssistantWidget::updateSerialPortList()
  */
 void SerialPortAssistantWidget::switchSerialPort()
 {
+    if (serialPort->isOpen()) {
+        serialPort->close();
+        ui->btn_switch->setText(tr("打开串口"));
+        enabledSerialPortConfig(true);
+        return;
+    }
     // 如果当前没有可用串口，将按钮 disable，同时直接返回
     if (previousPortList.isEmpty()) {
         return;
     }
+
     // 选择端口号
     serialPort->setPortName(ui->comboBox_serialPort->currentText());
     // 配置波特率
@@ -106,10 +136,15 @@ void SerialPortAssistantWidget::switchSerialPort()
     serialPort->setFlowControl(QSerialPort::FlowControl(ui->comboBox_flowCtrl->currentIndex()));
 
     if (serialPort->open(QIODeviceBase::ReadWrite)) {
-        qDebug() << "打开成功！";
         ui->btn_switch->setText(tr("关闭串口"));
+        enabledSerialPortConfig(false);
     } else {
-        qFatal() << "串口打开失败！";
+        QMessageBox msgBox(this);
+        msgBox.setIcon(QMessageBox::Warning);
+        msgBox.setWindowTitle(tr("错误"));
+        msgBox.setText(tr("打开串口失败！\n请检查指定串口是否存在或者已被打开"));
+        msgBox.addButton(tr("确定"), QMessageBox::AcceptRole);
+        msgBox.exec();
     }
 }
 
@@ -129,8 +164,6 @@ void SerialPortAssistantWidget::checkSerialPorts()
 
     // 如果串口列表发生变化
     if (currentPortList != previousPortList) {
-        qDebug() << "串口列表发生变化，更新列表...";
-
         // 保存当前选中的串口
         QString currentSelectedPort = ui->comboBox_serialPort->currentText();
 
@@ -157,5 +190,78 @@ void SerialPortAssistantWidget::checkSerialPorts()
         // 更新记录的上次串口列表
         previousPortList = currentPortList;
         qDebug() << "当前可用串口：" << currentPortList;
+    }
+}
+
+/**
+ * @brief SerialPortAssistantWidget::transmitData
+ * 发送数据
+ */
+void SerialPortAssistantWidget::transmitData()
+{
+    if (!serialPort->isOpen()) { // 检测串口是否打开，若没有打开则打开
+        switchSerialPort();
+    }
+    QString text = ui->lineEdit_transmitString->text();
+    if (!text.isEmpty()) {
+        QByteArray sendData = text.toUtf8();
+        sendData.append(char(0x03)); // 添加结束符 ETX
+        qint64 transmitBytes = serialPort->write(sendData);
+
+        if (transmitBytes == -1) {
+            ui->label_statusBar_sendStatus->setStyleSheet("color: red; font-weight: bold;");
+            ui->label_statusBar_sendStatus->setText(tr("发送失败！"));
+        } else {
+            // 获取当前时间并格式化
+            QDateTime currentTime = QDateTime::currentDateTime();
+            QString timestamp = currentTime.toString("[hh:mm:ss.zzz]");
+            // 创建带格式的文本
+            QString formattedText = QString("<span style=\"color:#98FB98;\">%1 &lt;- </span>%2")
+                                        .arg(timestamp, sendData.left(transmitBytes - 1));
+            // 使用 HEML 格式插入历史记录文本
+            ui->textEditRecord->append(formattedText);
+            // 计算发送数据的字节数
+            transmittedBytesTotal += transmitBytes - 1;
+            // 渲染发送结果状态
+            ui->label_statusBar_sendStatus->setStyleSheet("color: #00FF7F; font-weight: bold;");
+            ui->label_statusBar_sendStatus->setText(tr("发送成功！"));
+            ui->label_statusBar_sent->setText(tr("已发送: %1 字节").arg(transmittedBytesTotal));
+        }
+    } else {
+        ui->label_statusBar_sendStatus->setStyleSheet("color: orange; font-weight: bold;");
+        ui->label_statusBar_sendStatus->setText(tr("没有可发送的数据！"));
+    }
+}
+
+/**
+ * @brief SerialPortAssistantWidget::readyReadSerialPort
+ * 接收数据并加入缓冲区中
+ */
+void SerialPortAssistantWidget::readyReadSerialPort()
+{
+    // 将数据添加到缓冲区
+    receiveBuffer.append(serialPort->readAll());
+
+    int endIndex = receiveBuffer.indexOf(char(0x03)); 	// 查找结束符
+    if (endIndex == -1) { 	// 没有完整帧就等下一次触发
+        return;
+    }
+
+    // 取出一帧数据（不包含结束符）
+    QByteArray frameData = receiveBuffer.left(endIndex);
+    receiveBuffer.remove(0, endIndex + 1);
+
+    if (!frameData.isEmpty()) {
+        // 获取当前时间并格式化
+        QDateTime currentTime = QDateTime::currentDateTime();
+        QString timestamp = currentTime.toString("[hh:mm:ss.zzz]");
+        // 创建带格式的文本
+        QString formattedText = QString("<span style=\"color:#87CEEB;\">%1 -&gt; </span>%2")
+                                    .arg(timestamp, QString::fromUtf8(frameData));
+        // 使用 HTML 格式插入文本
+        ui->textEditReceive->append(formattedText);
+        int receivedLineBytes = frameData.size();
+        receivedBytesTotal += receivedLineBytes;
+        ui->label_statusBar_received->setText(tr("已接收: %1 字节").arg(receivedBytesTotal));
     }
 }
--- serialportassistantwidget.h
+++ serialportassistantwidget.h
@@ -25,12 +25,18 @@ private:
     QHash<QString, QSerialPort::StopBits> stopBitsMap; 	// 串口停止位选项映射
     QTimer* serialPortCheckTimer; 	// 串口检测定时器
     QStringList previousPortList; 	// 上一次检测到的串口列表
+    int transmittedBytesTotal; 	// 发送数据字节总数
+    int receivedBytesTotal; 	// 接收数据字节总数
+    QByteArray receiveBuffer; 	// 接收数据缓冲区
 
     void configMap(); 	// 串口配置映射
     void updateSerialPortList(); 	// 更新串口列表
+    void enabledSerialPortConfig(bool en); 	// 串口配置选项使能控制
 
 private slots:
-    void switchSerialPort();
+    void switchSerialPort(); 	// 开关串口
     void checkSerialPorts(); 	// 定时检测串口
+    void transmitData(); 	// 发送数据
+    void readyReadSerialPort();
 };
 #endif // SERIALPORTASSISTANTWIDGET_H
